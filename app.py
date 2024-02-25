27be735900769eca7988f328c3ff550acadf9e1d
KeilaCristine <keilacris.crento@gmail.com>
2024-02-25 18:48:28 -0300

    Inserindo avaliacoes

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
 restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais' 8)
-restaurante_praca.receber_avaliacao('Keila'7)
+restaurante_praca.receber_avaliacao('Lais', 8)
+restaurante_praca.receber_avaliacao('Keila', 7)
 
 
 def main():
