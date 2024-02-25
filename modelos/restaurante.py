0c19e28316c4cd464df1b5120384dd9f0bb28fea
KeilaCristine <keilacris.crento@gmail.com>
2024-02-25 19:18:54 -0300

    Inserindo media avalaiçoes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -5,19 +5,19 @@ class Restaurante:
 
     def __init__(self, nome, categoria):
         self._nome = nome.title()
-        self.categoria = categoria.upper()
+        self._categoria = categoria.upper()
         self._ativo = False
-        self.avaliacao = []
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
-
+    
     def __str__(self):
-        return f'{self._nome} | {self.categoria}'
-        
+        return f'{self._nome} | {self._categoria}'
+    
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliacao'.ljust(25)} | {'Status'.ljust(25)}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
     @property
     def ativo(self):
@@ -28,13 +28,13 @@ class Restaurante:
 
     def receber_avaliacao(self, cliente, nota):
         avaliacao = Avaliacao(cliente, nota)
-        self.avaliacao.append(avaliacao)
-        
+        self._avaliacao.append(avaliacao)
+
     @property
     def media_avaliacoes(self):
-        if not self.avaliacao:
+        if not self._avaliacao:
             return 0
-        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avalicao)
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
